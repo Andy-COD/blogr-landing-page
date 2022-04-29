b07f1a63c6933e9dc6de96135ab954f744dc60dd
Andy <aahwireng10@gmail.com>
2022-04-29 21:09:28 +0000

    Fixed dropdown pushing the header down but caused an error to the mobile dropdown

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 const dropdown = document.querySelectorAll(".dropbtn");
-// const arrow = document.querySelectorAll(".arrow");
 const icon = document.querySelector(".mobile-menu");
+const dropDownContent = document.querySelectorAll('.dropdown-content');
 
+
+// Hambuger animation
 icon.addEventListener("click", () => {
     let img = document.querySelector("#hamburger");
     let menus = document.querySelector(".mobile-menus");
@@ -14,10 +16,30 @@ icon.addEventListener("click", () => {
         img.setAttribute("src", "/images/icon-hamburger.svg");
     }
 });
-
+// show dropdown and rotate arrow
 dropdown.forEach(item => {
     item.addEventListener("click", (e) => {
-        let content = e.target.nextElementSibling;
-        content.classList.toggle("show-links");
+        let content = e.target.nextElementSibling.nextElementSibling;
+        let arrow = e.target.nextElementSibling;
+        let rotateUp = 0;
+        let rotateDown = 0;
+        if(!content.classList.contains('show-links')) {
+            content.classList.add('show-links')
+            arrow.style.transform = `rotate(${rotateUp + 180}deg)`
+        }else {
+            content.classList.remove('show-links');
+            arrow.style.transform = `rotate(${rotateDown - 1}deg)`
+        }
+        
     });
 });
+// CLose all dropdowns on window click
+window.addEventListener('mouseup', (e) => {
+    if(!e.target.classList.contains("dropdown") && !e.target.parentNode.classList.contains("dropdown")) {
+        dropDownContent.forEach(item => {
+            if(item.classList.contains('show-links')) {
+                item.classList.remove('show-links')
+            }
+        })
+    }
+})
